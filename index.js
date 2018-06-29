766ad4a4a108272f98e95c2d8ef56440252d2369
Donna Cerrio <denique86@yahoo.com>
2018-06-29 02:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 var greeting = "Hello, everybody!";
+=======
+var greeting = "Hello, Everybody";
+>>>>>>> 0b03d7f8e25a146c31b577a6210751ba70db8cc5
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = `Hello, + ${specialGuest}!`
+
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = (`Let's talk about ${topic}`)
